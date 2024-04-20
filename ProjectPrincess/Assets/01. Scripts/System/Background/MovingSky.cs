213a1847406c67087b9c49344e12d2a7941b1e4d
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-04-21 02:22:50 +0900

    sky scroll

--- ProjectPrincess/Assets/01. Scripts/System/Background/MovingSky.cs
+++ ProjectPrincess/Assets/01. Scripts/System/Background/MovingSky.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class MovingSky : MonoBehaviour
 {
-    [SerializeField] private float scrollSpeed = 0.1f; 
+    [SerializeField] private float scrollSpeed = 0.1f;
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform parent;
+
     private Renderer renderer;
     private Material material;
 
@@ -16,6 +20,10 @@ public class MovingSky : MonoBehaviour
 
     private void Update()
     {
+        Vector3 newPosition = parent.position;
+        newPosition.x = player.position.x;
+        parent.position = newPosition;
+
         float offsetX = Time.time * scrollSpeed;
         Vector2 offset = new Vector2(offsetX, 0);
         material.mainTextureOffset = offset;
